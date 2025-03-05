40072f7d5c719a8f16bb412b6617d509713a078d
unknown <105788640+idgf-20@users.noreply.github.com>
2025-03-05 19:47:44 +0100

    dfsfgsdg

--- src/komponenten/Auswahl.js
+++ src/komponenten/Auswahl.js
@@ -29,14 +29,14 @@ const Auswahl = () => {
   
 
   const fetchTimetable = (userId) => {
-    fetch(`${process.env.REACT_APP_API_BASE_URL}/api/timetable?userId=${userId}`)
+    fetch(`https://apis-1u1x.onrender.com/api/timetable?userId=${userId}`)
       .then((response) => response.json())
       .then((data) => setTimetable(data))
       .catch((error) => console.error('Fehler beim Laden des Stundenplans:', error));
   };
 
   const fetchCourseColors = (userId) => {
-    fetch(`${process.env.REACT_APP_API_BASE_URL}/api/courseColors?userId=${userId}`)
+    fetch(`https://apis-1u1x.onrender.com/api/courseColors?userId=${userId}`)
       .then((response) => response.json())
       .then((data) => setCourseColors(data))
       .catch((error) => console.error('Fehler beim Laden der Farben:', error));
@@ -49,7 +49,7 @@ const Auswahl = () => {
       [course]: color,
     }));
   
-    fetch('http://localhost:3000/api/courseColor', {
+    fetch('https://apis-1u1x.onrender.com/api/courseColor', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ userId, course, color }),
@@ -68,7 +68,7 @@ const Auswahl = () => {
     setTimetable((prevTimetable) => prevTimetable.filter((entry) => entry.id !== id));
 
     // Dann die Löschanfrage an den Server senden
-    fetch(`${process.env.REACT_APP_API_BASE_URL}/api/timetable/${id}`, {
+    fetch(`https://apis-1u1x.onrender.com/api/timetable/${id}`, {
       method: 'DELETE',
     })
       .then((response) => {
